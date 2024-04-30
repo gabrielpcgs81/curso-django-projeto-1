3bb584a219a94696f1b0cce45272db72f6e36716
Gabriel Seabra <gabrielpcgs81@gmail.com>
2024-04-30 07:25:12 -0300

    App recipes e entendendo funcionamento func render, templates e views

---
+++ recipes/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'recipes/home.html', context ={'nome':'Gabriel'})
+
+def sobre(request):
+    return HttpResponse("sobre")
+
+def contato(request):
+    return render(request, 'recipes/contato.html')
